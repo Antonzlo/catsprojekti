5af94dbbd5b862638acf4c9493d39c49ef88cded
szzphi <leezanyaaa@gmail.com>
2024-10-31 14:33:49 +0200

    homepagealku

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header'; 
+import HomePage from './Homepage';
 import './App.css'
 
 const App = () => {
     return (
         <div>
             <Header />
+            <HomePage/>
             {/* Other components can go here */}
         </div>
     );
