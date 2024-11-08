1bb24004a3bc2e01e873a620c1fc93aa24363e13
Anja <s2200751@edu.bc.fi>
2024-11-08 12:05:26 +0200

    fixed

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const { port, host } = require('./config.json');
 const app = express();
+const cors = require('cors');
+const mysql = require('mysql2');
 
-// Import routes
-require('./database')(app); // Pass `app` to the database file
+const dbConfig = require('./dbconfig.json');
+const { port, host } = require('./config.json');
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +14,55 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'templates'));
 
-// Start the server
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error', err);
+    return;
+  }
+});
+
+app.get('/cats', (req, res) => {
+  const query = 'SELECT * FROM cats';
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('error', err);
+      res.status(500).send('server error');
+      return;
+    }
+
+    res.json(results);
+  });
+});
+
+app.get('/colours', (req, res) => {
+    const query = 'SELECT DISTINCT color FROM cats';
+  
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.error('error', err);
+        res.status(500).send('server error');
+        return;
+      }
+  
+      res.json(results);
+    });
+  });
+
+  app.get('/size', (req, res) => {
+    const query = 'SELECT DISTINCT size FROM cats';
+    connection.query(query, (err, results) => {
+      if (err) {
+        console.error('Query error:', err);
+        res.status(500).send('Server error');
+        return;
+      }
+      res.json(results);
+    });
+  });
+
 app.listen(port, host, () => {
   console.log(`cats projekti toimii ${host}:${port}`);
 });
